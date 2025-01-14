baf149aa57aeb91a3fa0c629fe37779a46a7ceb1
sylim <sylim@danbiedu.co.kr>
2025-01-14 13:37:00 +0900

    Setting: Entity & AggregateRoot
    - common 모듈 추가
    - BaseEntity 설정
    - AggregateRoot 설정

---
+++ common/common-domain/src/main/java/com.food.ordering.system.domain/entity/AggregateRoot.java
@@ -0,0 +1,15 @@
+package com.food.ordering.system.domain.entity;
+
+/**
+ * packageName    : com.food.ordering.system.domain.entity
+ * fileName       : AggregateRoot
+ * author         : limsooyoung
+ * date           : 1/14/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 1/14/25        limsooyoung       최초 생성
+ */
+public abstract class AggregateRoot<ID> extends BaseEntity<ID>{
+}
