3d6a83f9da0e5bee1b4dc9a573dfdabd4cfc0078
sylim <sylim@danbiedu.co.kr>
2025-01-15 10:31:32 +0900

    MOD: 주문 비즈니스 로직 구현
    - 주문 상태 처리

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,18 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import com.food.ordering.system.domain.entity.AggregateRoot;
+import com.food.ordering.system.domain.valueobject.CustomerId;
+
+/**
+ * packageName    : com.food.ordering.system.order.service.domain.entity
+ * fileName       : Customer
+ * author         : limsooyoung
+ * date           : 1/15/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 1/15/25        limsooyoung       최초 생성
+ */
+public class Customer extends AggregateRoot<CustomerId> {
+}
