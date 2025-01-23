0556ab780584e98ecb1317f600606af2989d85d2
sylim <sylim@danbiedu.co.kr>
2025-01-23 15:04:39 +0900

    ADD: 주문 애플리케이션 서비스에서 이벤트 실행을 위한 메시지 게시자 구현
    1. createOrder에서 트랜잭션을 포함한 작업이 실행되고, 그 후 publish가 호출되어 OrderCreatedEvent 발행
    2. ApplicationDomainEventPublisher가 이벤트를 발행하지만, 그 처리는 트랜잭션이 커밋될 때까지 대기
    3. @TransactionalEventListener가 트랜잭션의 커밋 후에 이벤트를 가로채고, 트랜잭션이 완료된 후에 관련된 후속 작업을 수행

---
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ApplicationDomainEventPublisher.java
@@ -0,0 +1,40 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * packageName    : com.food.ordering.system.order.service.domain
+ * fileName       : ApplicationDomainEventPublisher
+ * author         : limsooyoung
+ * date           : 1/22/25
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 1/22/25        limsooyoung       최초 생성
+ */
+@Slf4j
+@Component
+public class ApplicationDomainEventPublisher implements
+        ApplicationEventPublisherAware,
+        DomainEventPublisher<OrderCreatedEvent> {
+
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.applicationEventPublisher = applicationEventPublisher;
+    }
+
+    @Override
+    public void publish(OrderCreatedEvent domainEvent) {
+        this.applicationEventPublisher.publishEvent(domainEvent);
+        log.info("OrderCreatedEvent is published for order id: {}", domainEvent.getOrder()
+                .getId().getValue());
+    }
+}
